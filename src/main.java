818d6d78e7f5c9187c4f49fe778afd8b81c874c0
Truth <thetruth0906@livemail.tw>
2016-03-20 19:55:07 +0800

    First commit

---
+++ src/main.java
@@ -0,0 +1,58 @@
+import java.security.SecureRandom;
+
+import uk.ac.ic.doc.jpair.api.FieldElement;
+import uk.ac.ic.doc.jpair.api.Pairing;
+import uk.ac.ic.doc.jpair.pairing.BigInt;
+import uk.ac.ic.doc.jpair.pairing.PairingFactory;
+import uk.ac.ic.doc.jpair.pairing.Point;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		Pairing e = PairingFactory.ssTate(160, 256, new SecureRandom());
+		Point P=e.RandomPointInG1(new SecureRandom());//randomly choose a point P
+		
+		BigInt A = new BigInt("2");
+		BigInt B = new BigInt("3");
+		BigInt A_B = A.add(B);
+		
+		System.out.println("A = " + A);
+		System.out.println("B = " + B);
+		
+		Point AP = e.getCurve().multiply(P, A);
+		Point BP = e.getCurve().multiply(P, B);
+		
+		System.out.println("P:" + P);
+		System.out.println("A x P:" + AP);
+		System.out.println("B x P:" + BP);
+		
+		Point BAP = e.getCurve().multiply(AP, B);
+		Point ABP = e.getCurve().multiply(BP, A);
+		
+		System.out.println("B x A x P:" + BAP);
+		System.out.println("A x B x P:" + ABP);
+		
+		
+		System.out.println("e((A+B)P, P) =? e(AP, P)e(BP, P)");
+		
+		Point A_BP = e.getCurve().multiply(P, A_B);
+		
+		FieldElement Result_A = e.compute(A_BP, P);
+		
+		FieldElement Result_B_1 = e.compute(AP, P);
+		FieldElement Result_B_2 = e.compute(BP, P);
+		
+		FieldElement Result_B = e.getGt().multiply(Result_B_1, Result_B_2);
+		
+		System.out.println("e((A+B)P, P)    : " + Result_A);
+		System.out.println("e( A   P, P)    : " + Result_B_1);
+		System.out.println("e(   B P, P)    : " + Result_B_2);
+		System.out.println("e(AP, P)e(BP, P): " + Result_B);
+		
+		System.out.println("e((A+B)P, P) == e(AP, P)e(BP, P): " + Result_A.equals(Result_B));
+		
+	}
+
+}
